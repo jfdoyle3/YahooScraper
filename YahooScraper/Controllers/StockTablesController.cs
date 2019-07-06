17956eead47a77b4560eee0c75d24af8935e60f0
Jim Doyle <indellibleink@gmail.com>
2019-07-06 09:29:19 -0400

    Have database dispalying on web page

---
+++ YahooScraper/Controllers/StockTablesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using YahooScraper.Models;
+
+namespace YahooScraper.Controllers
+{
+    public class StockTablesController : Controller
+    {
+        private StocksContext db = new StocksContext();
+
+        // GET: StockTables
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.StockTables.ToListAsync());
+        }
+
+        // GET: StockTables/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockTable stockTable = await db.StockTables.FindAsync(id);
+            if (stockTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockTable);
+        }
+
+        // GET: StockTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StockTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,DateStamp,Symbol,LastPrice,Change,ChgPc,Currency,MarketTime,Volume,AvgVol3m,MarketCap")] StockTable stockTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StockTables.Add(stockTable);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(stockTable);
+        }
+
+        // GET: StockTables/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockTable stockTable = await db.StockTables.FindAsync(id);
+            if (stockTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockTable);
+        }
+
+        // POST: StockTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,DateStamp,Symbol,LastPrice,Change,ChgPc,Currency,MarketTime,Volume,AvgVol3m,MarketCap")] StockTable stockTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stockTable).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(stockTable);
+        }
+
+        // GET: StockTables/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockTable stockTable = await db.StockTables.FindAsync(id);
+            if (stockTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockTable);
+        }
+
+        // POST: StockTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            StockTable stockTable = await db.StockTables.FindAsync(id);
+            db.StockTables.Remove(stockTable);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
