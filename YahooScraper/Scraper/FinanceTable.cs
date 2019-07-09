e666cf288de9ca8b421006f2d145efb986fc9cc6
Jim Doyle <indellibleink@gmail.com>
2019-07-09 00:41:29 -0400

    added scraper classes to project

---
+++ YahooScraper/Scraper/FinanceTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ConsoleScraper
+{
+   public class FinanceTable
+    {
+        public static void ScrapeToDatabase(List<List<string>> stockData)
+        {
+            using (StocksContext db = new StocksContext())
+            {
+                
+                for (int i = 0; i < stockData.Count; i++)
+                {
+                    
+                    StockTable stockTable = new StockTable
+                    {
+                        DateStamp=DateTime.Now,
+                        Symbol = stockData[i][0].ToString(),
+                        LastPrice = stockData[i][1].ToString(),
+                        Change = stockData[i][2].ToString(),
+                        ChgPc = stockData[i][3].ToString(),
+                        Currency = stockData[i][4].ToString(),
+                        MarketTime = stockData[i][5].ToString(),
+                        Volume = stockData[i][6].ToString(),
+                        AvgVol3m = stockData[i][8].ToString(),
+                        MarketCap = stockData[i][12].ToString(),
+                    };
+
+                    db.StockTables.Add(stockTable);
+                    db.SaveChanges();
+                }
+                
+                Console.WriteLine("\n\nStock data entered into the database.");
+                
+            }
+        }
+    }
+}
---
+++ YahooScraper/Scraper/FromFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HtmlAgilityPack;
+using System.IO;
+using System.Linq;
+using System.Data;
+
+
+namespace ConsoleScraper
+{
+    public class FromFile
+    {
+        public List<List<string>> ReadFile()
+        {
+            string stocks = File.ReadAllText(@"C:\website\Stocks.htm");
+            HtmlDocument yahooPage = new HtmlDocument();
+
+            yahooPage.LoadHtml(stocks);
+
+
+            List<List<string>> stockTable =
+                                    yahooPage.DocumentNode.SelectSingleNode("//table")
+                                                .Descendants("tr")
+                                                .Skip(1) 
+                                                .Where(tr => tr.Elements("td").Count() > 1)
+                                                .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                                                .ToList();
+
+            return stockTable;
+        }
+    }
+}
